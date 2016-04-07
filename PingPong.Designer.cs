80893bcfba70b3bfa9ccccfc4e9cc00820687cba
Vegh Adam <veghadam1991@gmail.com>
2016-04-07 19:49:11 +0200

    add start page and basic racket

---
+++ PingPong.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PingPongGame
+{
+	partial class formPingPong
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.buttonExit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.buttonStart.Location = new System.Drawing.Point(167, 131);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(166, 68);
+			this.buttonStart.TabIndex = 0;
+			this.buttonStart.Text = "Start";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 50;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// buttonExit
+			// 
+			this.buttonExit.Font = new System.Drawing.Font("Segoe Print", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.buttonExit.Location = new System.Drawing.Point(167, 241);
+			this.buttonExit.Name = "buttonExit";
+			this.buttonExit.Size = new System.Drawing.Size(166, 68);
+			this.buttonExit.TabIndex = 1;
+			this.buttonExit.Text = "Exit";
+			this.buttonExit.UseVisualStyleBackColor = true;
+			this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+			// 
+			// formPingPong
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 461);
+			this.Controls.Add(this.buttonExit);
+			this.Controls.Add(this.buttonStart);
+			this.Name = "formPingPong";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Do you wanna play with me? Click on me!";
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormPingPong_MouseMove);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button buttonExit;
+	}
+}
+
---
+++ PingPong.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongGame
+{
+	public partial class formPingPong : Form
+	{
+		int xRec = 150;
+		int yRec = 400;
+		int widthRec = 200;
+		int height = 20;
+
+		int xEll = 150;
+		int yEll = 100;
+
+		public formPingPong()
+		{
+			InitializeComponent();
+		}
+
+		private void DrawIt(int xRed, int yRec, int xEll, int yEll)
+		{
+			this.Text = "PING PONG GAME";
+			Graphics g = this.CreateGraphics();
+			g.Clear(Color.White);
+			Rectangle r = new Rectangle(xRec, yRec, 200, 20);
+			Rectangle ball = new Rectangle(xEll, yEll, 80, 80);
+
+			g.FillRectangle(Brushes.Cyan, r);
+			g.DrawRectangle(Pens.Red, r);
+			g.FillEllipse(Brushes.Coral, ball);
+			g.DrawEllipse(Pens.BlueViolet, ball);
+		}
+
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+			buttonStart.Hide();
+			buttonExit.Hide();
+			DrawIt(xRec, yRec, xEll, yEll);
+			timer1.Enabled = true;
+		}
+
+		private void buttonExit_Click(object sender, EventArgs e)
+		{
+			var box = MessageBox.Show("Are you sure?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+			if (box == DialogResult.No)
+				return;
+			MessageBox.Show("Then F*CK YOU!", "F*CK YOU!");
+			Close();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			yEll += 10;
+			DrawIt(xRec, yRec, xEll, yEll);
+		}
+
+		private void FormPingPong_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (e.X <= widthRec / 2)
+				xRec = 0;
+			else if (e.X >= this.Width - widthRec / 2)
+				xRec = this.Width - widthRec;
+			else
+				xRec = e.X - widthRec/2;
+		}
+	}
+}
